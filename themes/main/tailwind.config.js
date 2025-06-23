8666ff0672f396bdcd0724776cd9d549c5860de5
Dario <dario@kara5.mk>
2025-06-23 14:50:47 +0200

    layout changes / spacing / footer changes / hover gespi-cta changes

--- themes/main/tailwind.config.js
+++ themes/main/tailwind.config.js
@@ -8,6 +8,25 @@ module.exports = {
         "./assets/components/*.vue",
     ],
     theme: {
+        screens: {
+            'xs': '420px',
+            // => @media (min-width: 320px) { ... }
+
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
         extend: {
             container: {
                 center: true,
@@ -18,6 +37,7 @@ module.exports = {
                 }
             },
         },
+
     },
     extend: {},
     plugins: [],
